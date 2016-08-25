38a7fde299724b931348df987f534c0cad1caa29
efarfan2393 <e.farfanvaldivia@gmail.com>
2016-08-25 14:37:33 -0500

    subiendo ejercicio dos

---
+++ app.js
@@ -0,0 +1,5 @@
+var num1 = prompt("Escribe un número");
+var num2 = prompt("Escribe un número");
+var num3 = prompt("Escribe un número");
+var resultado = (parseInt(num1) + parseInt(num2) + parseInt(num3))/3;
+window.alert(resultado);
